01606f7eb03e67bd4215b9ca4e13a3af3aa6ddbb
Nils <nilsb@darkstar.(none)>
2011-11-15 23:14:32 +0100

    basic API and demos for Metropolis Hastings monad

---
+++ src/probabilistic_clojure/monadic/api.clj
@@ -0,0 +1,237 @@
+(ns
+    ^{:author "Nils Bertschinger"
+      :doc "This library implements a version of the probability monad
+which uses Metropolis Hastings sampling.
+The system allows to condition and memoize probabilistic choice points and
+can be extended by user defined distributions."}
+  probabilistic-clojure.monadic.api
+  (:use [clojure.contrib.monads :only (defmonad)]
+	[probabilistic-clojure.utils.sampling :only (sample-from)]
+	[probabilistic-clojure.utils.stuff :only (ensure-list error)]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Basic data structures
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defrecord ^{:doc "Basic data structure for random choice points"}
+  ChoicePoint
+  [sampler get-log-prob proposer get-log-proposal-prob])
+
+(defrecord ^{:doc "Entry holding information about random choices encountered
+during a run of probabilistic programs"}
+  DBentry
+  [value log-lik status type params choice-point])
+
+(defn- activate [database addr]
+  (assoc-in database [addr :status] :active))
+
+(defn- inactivate-all [database]
+  (into {} (for [[addr entry] database]
+	     [addr (assoc entry :status :inactive)])))
+
+(defn- clean-db-back
+  "Runs through the database to remove all inactive choice points. Returns the cleaned
+database as well as the log-likelihood of the removed choice points which is needed to
+calculate the backward probability"
+  [database]
+  (reduce (fn [[db log-bwd-prob] [addr entry]]
+	    (if (= (:status entry) :active)
+	      [(assoc db addr entry) log-bwd-prob]
+	      [db (+ log-bwd-prob (apply (:get-log-prob (:choice-point entry))
+					 (:value entry) (:params entry)))]))
+	  [{} 0] (seq database)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; The monadic interface
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def log-prob-zero (Math/log 0))
+
+(defn- m-result-MH [v]
+  (fn [addr database log-fwd-prob log-lik mems]
+    ;; there is no choice here, so just return everything directly
+    [v log-lik database log-fwd-prob mems]))
+
+(def m-zero-MH
+  (fn [addr database log-fwd-prob log-lik mems]
+    ;; invalidate this trace
+    [::invalid log-prob-zero database log-fwd-prob mems]))
+
+(defn invalid? [v]
+  (= v ::invalid))
+
+(defn- m-bind-MH [m f]
+  (fn [addr database log-fwd-prob log-lik mems]
+    ;; here we first run the monad m and then
+    ;; plug the values into the continuation f
+    ;; (compare the state monad)
+    (let [[v log-lik database log-fwd-prob mems]
+	  (m (cons "BIND-M" addr) database log-fwd-prob log-lik mems)]
+      (if (invalid? v)
+	(m-zero-MH (cons "BIND-F" addr) database log-fwd-prob log-lik mems)
+	((f v) (cons "BIND-F" addr) database log-fwd-prob log-lik mems)))))
+
+;; Definition of our monad (note that no implementation of m-plus is provided!)
+(defmonad probabilistic-sampling-m
+  [m-result m-result-MH
+   m-bind   m-bind-MH
+   m-zero   m-zero-MH])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; User functions for defining choice points and running probabilistic programs
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn make-choice-point
+  "Allows to define a new probabilistic choice point by providing a function
+for sampling and calculating the log-likelihood.
+See the source code of flip for an example."
+  ([name params sampler get-log-prob]
+     (make-choice-point name params
+			sampler get-log-prob
+			;; provide a default proposer that simply samples from the distribution
+			(fn [val & args] (apply sampler args))
+			(fn [new-val old-val & args] (apply get-log-prob new-val args))))
+  ([name params sampler get-log-prob proposer get-log-proposal-prob]
+     (fn [addr database log-fwd-prob log-lik mems]
+       ;; this function runs the choice-point with entries from the database and updates the trace
+       (let [addr (cons name addr)
+	     create-new-randomness
+	     (fn []
+	       (let [val (apply sampler params)
+		     ll  (apply get-log-prob val params)]
+		 [val
+		  (+ log-lik ll)
+		  (assoc database addr
+			 (DBentry. val ll :active name params
+				   (ChoicePoint. sampler get-log-prob proposer get-log-proposal-prob)))
+		  (+ log-fwd-prob ll) mems]))]				  
+	 (if (contains? database addr)
+	   (let [entry (database addr)]
+	     (if (= (:params entry) params)
+	       ;; we found an exact match, so just lookup the value
+	       (let [val (:value entry)
+		     ll  (apply (:get-log-prob (:choice-point entry)) val params)]
+		 [val (+ log-lik ll) (activate database addr) log-fwd-prob mems])
+	       ;; no exact match, we have to reweight the trace
+	       (let [val (:value entry)
+		     ll  (apply (:get-log-prob (:choice-point entry)) val params)]
+		 (if (= ll log-prob-zero)
+		   (create-new-randomness)
+		   [val (+ log-lik ll)
+		    (-> database
+			(activate addr)
+			(assoc-in [addr :log-lik] ll)
+			(assoc-in [addr :params]  params))
+		    log-fwd-prob mems]))))
+	   ;; not in database, create new randomness
+	   (create-new-randomness))))))
+
+(defn- get-trace [m-MH database]
+  (m-MH (list "TOP") database 0 0 {}))
+
+(defn cond-data
+  "Condition a choice point on the specified value.
+This function must be applied to an elementary random choice point, e.g. (flip 0.6).
+A trace running through this value is then weighted according to the likelihood of the data value."
+  [choice val]
+  (fn [addr database log-fwd-prob log-lik mems]
+    (let [[[_ choice-entry] & more] (seq (get (get-trace choice {}) 2))
+	  choice-point (:choice-point choice-entry)
+	  ll (apply (:get-log-prob choice-point) val (:params choice-entry))]
+      (when more    ; ensure that its was called on an elementary choice point
+	(error "Cond-data can only be called on an elementary choice point!"))
+      [val (+ log-lik ll) database log-fwd-prob mems])))
+
+(defmacro mem
+  "Memoize a MH monadic value on some given arguments, i.e. if the same arguments
+are encountered again it refers back to the original choice
+point which was established for these arguments in the trace, i.e.
+no new randomness is created in this case."
+  [m-MH-form & add-args]
+  `(fn [addr# database# log-fwd-prob# log-lik# mems#]
+     (let [mem-addr# (list "MEMO" (str '~(first (ensure-list m-MH-form))) ~@(rest (ensure-list m-MH-form)) ~@add-args)
+	   ;; now just redirect the choice point to the new address
+	   ;; which identifies it according to its arguments
+	   [val# ll# db# lfp# ms#] (~m-MH-form mem-addr# database# log-fwd-prob# log-lik# mems#)]
+       (if (contains? mems# mem-addr#)
+	 (do ;; (assert (= database# db#))
+	   [val# log-lik# database# log-fwd-prob# (update-in ms# [mem-addr#] inc)])
+	 [val# ll# db# lfp# (assoc ms# mem-addr# 1)]))))
+
+(defn sample-traces
+  "The main routine implementing Metropolis Hastings sampling. Returns a lazy
+sequence of samples when called on a monadic value from this library.
+Burn-in and thinning can be obtained using for example drop and keep-indexed respectively."
+  ([m-MH]
+     (println "Trying to find valid trace ...")
+     (loop [[val log-lik database mems] (get-trace m-MH {})]
+       (if (or (invalid? val) (= log-lik log-prob-zero))
+	 (recur (get-trace m-MH {}))
+	 (do (println "Starting MH-sampling.")
+	     (sample-traces m-MH database mems log-lik val 1 1)))))
+  ([m-MH database mems log-lik val idx num-acc]
+     (when (= (mod idx 500) 0)
+       (println (str idx ": value " val " with log. likelihood " log-lik))
+       (println (str "Accepted " num-acc " out of last 500 proposals")))
+     (let [num-acc (if (= (mod idx 500) 0) 0 num-acc)
+	   [addr entry] (rand-nth (for [[addr entry] database,
+					en (repeat (if (contains? mems (rest addr))
+						     (mems (rest addr))
+						     1)
+						   entry)]
+				    [addr en]))
+	   prop-val    (apply (:proposer (:choice-point entry)) (:value entry) (:params entry))
+	   ll-prop-val (apply (:get-log-proposal-prob (:choice-point entry))
+			      prop-val (:value entry) (:params entry))
+	   ll-val      (apply (:get-log-proposal-prob (:choice-point entry))
+			      (:value entry) prop-val (:params entry))
+	   [next-val next-log-lik next-database log-fwd-prob next-mems]
+	   (get-trace m-MH
+		      (-> (inactivate-all database)
+			  (assoc-in [addr :value] prop-val)
+			  (assoc-in [addr :log-lik]
+				    (apply (:get-log-prob (:choice-point entry)) prop-val (:params entry)))))]
+       (if (invalid? next-val) ; trace was rejected => retry
+	 (lazy-seq (cons val (sample-traces m-MH database mems log-lik val (inc idx) num-acc)))
+	 ; (recur m-MH database log-lik val)
+	 (let [[next-database log-bwd-prob] (clean-db-back next-database)]
+	   (if (< (Math/log (rand))
+	   	  (+ (- next-log-lik log-lik)
+	   	     (Math/log (/ (count database) (count next-database)))
+	   	     (- ll-val ll-prop-val)
+	   	     (- log-bwd-prob log-fwd-prob)))
+	     (lazy-seq (cons next-val (sample-traces m-MH next-database next-mems
+						     next-log-lik next-val
+						     (inc idx) (inc num-acc))))
+	     (lazy-seq (cons val (sample-traces m-MH database mems log-lik val (inc idx) num-acc)))))))))
+
+(defn monte-carlo-samples
+  "Run the probabilistic program repeatedly to obtain samples. Note that this is more like
+rejection sampling and usually less efficient than sample-traces."
+  [m-MH]
+  (lazy-seq
+   (cons (first (get-trace m-MH {}))
+	 (monte-carlo-samples m-MH))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Basic example of how to define a probabilistic choice point
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn flip
+  "A binary random choice which is true with probability p.
+Provides a good example of how user-defined choice points look like."
+  [p]
+  (make-choice-point "FLIP" [p]
+		     (fn [p] (< (rand) p))  ; the sampling function
+		     (fn [b p] (Math/log (if b p (- 1 p)))) ; calculates the log-likelihood of outcome b
+		     ;; The proposer (consisting of theses two functions) is optional
+		     (fn [b p] (not b))
+		     (fn [new-b old-b p] (Math/log 1))))
---
+++ src/probabilistic_clojure/monadic/demos.clj
@@ -0,0 +1,226 @@
+(ns
+    ^{:author "Nils Bertschinger"
+      :doc "Demo of how the probability monad can be used.
+Also defines several useful probabilistic choice points and
+requires incanter for different distributions as well as the
+graphical output."}
+  probabilistic-clojure.monadic.demos
+  (:use [probabilistic-clojure.monadic.api
+	 :only (probabilistic-sampling-m make-choice-point cond-data mem sample-traces flip log-prob-zero)]
+	[probabilistic-clojure.utils.sampling :only (density sample-from)]
+	[probabilistic-clojure.utils.stuff :only (transpose)])
+  (:use [clojure.contrib.monads :only (domonad m-bind m-result with-monad)])
+  (:use [incanter.core :only (gamma view)]
+	[incanter.stats :only (sample-normal pdf-normal sample-dirichlet sample-beta pdf-beta mean)]
+	[incanter.charts :only (histogram xy-plot add-lines)]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; First a simple Bayes net as found in many introductory texts
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn noisy-or [x y]
+  (domonad probabilistic-sampling-m
+   [noise-x (flip 0.9)
+    noise-y (flip 0.8)
+    noise-z (flip 0.1)]
+   (or (and x noise-x)
+       (and y noise-y)
+       noise-z)))
+
+(defn grass-bayes-net []
+  (domonad probabilistic-sampling-m
+   [rain      (flip 0.3)
+    sprinkler (flip 0.5)
+    grass-is-wet (noisy-or rain sprinkler)
+    :when grass-is-wet]
+   rain))
+
+(defn run-grass-example [num-samples]
+  (let [rain (density (take num-samples (sample-traces (grass-bayes-net))))]
+    (println "Given grass-is-wet the probability for rain is " (get rain true))
+    rain))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Some more probabilistic choice points as required by the mixture example
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn discrete [dist]
+  (make-choice-point "DISCRETE" [dist]
+		     (fn [dist] (sample-from dist))
+		     (fn [x dist]
+		       (if (contains? dist x)
+			 (Math/log (dist x))
+			 log-prob-zero))))
+
+(defn gaussian [mu sdev]
+  (let [proposal-sd 0.7]
+    (make-choice-point "GAUSSIAN" [mu sdev]
+		       (fn [mu sdev] (sample-normal 1 :mean mu :sd sdev))
+		       (fn [x mu sdev] (Math/log (pdf-normal x :mean mu :sd sdev)))
+		       (fn [x mu sdev] (sample-normal 1 :mean x :sd proposal-sd))
+		       (fn [new-x old-x mu sdev]
+			 (Math/log (pdf-normal new-x :mean old-x :sd proposal-sd 0.7))))))
+
+(defn pdf-dirichlet [ps alphas]
+  (let [norm (/ (reduce * (map gamma alphas))
+		(gamma (reduce + alphas)))]
+    (/ (reduce * (map (fn [p a] (Math/pow p (- a 1))) ps alphas))
+       norm)))
+
+(defn dirichlet [alphas]
+  (let [proposal-alphas (fn [alphas]
+			  (for [a alphas] (* 18 a)))]
+    (make-choice-point "DIRICHLET" [alphas]
+		       (fn [alphas] (first (sample-dirichlet 2 alphas)))
+		       (fn [ps alphas] (pdf-dirichlet ps alphas))
+		       (fn [ps alphas] (first (sample-dirichlet 2 (proposal-alphas ps))))
+		       (fn [new-ps old-ps alphas]
+			 (Math/log (pdf-dirichlet new-ps (proposal-alphas old-ps)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Fitting of a mixture model to illustrate how mem can be used to share randomness
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn generate-data []
+  (let [mu (fn [] (sample-from {-5 0.2, 0 0.7, 8 0.1}))]
+    (lazy-seq (cons (sample-normal 1 :mean (mu))
+		    (generate-data)))))
+
+(def data (take 42 (generate-data)))
+
+(defn fit-components [data comp-weights mu-comps]
+  (with-monad probabilistic-sampling-m
+    (if (empty? data) ; are we done yet?
+      (m-result ())
+      (domonad
+       [comp (discrete {:a (nth comp-weights 0), :b (nth comp-weights 1), :c (nth comp-weights 2)})
+	;; first draw a component for this data point
+	:let [mu-comp (mu-comps comp)]
+	;; then condition a Gaussian for this component on the observed data point
+	obs  (cond-data (gaussian mu-comp 1) (first data))
+	assignment (fit-components (rest data) comp-weights mu-comps)]
+       (cons comp assignment)))))
+
+(defn mixture
+  "Basic example of a mixture model."
+  [data]
+  (domonad probabilistic-sampling-m
+    [ca (gaussian 0 15) ; draw means of three components from a Gaussian prior
+     cb (gaussian 0 15)
+     cc (gaussian 0 15)
+     comp-weights (dirichlet [1 1 1]) ; draw component proportions from a Dirichlet prior
+     :let [mu-comps {:a ca :b cb :c cc}]
+     assignment (fit-components data comp-weights mu-comps)] ; assign each data point to these components
+    [mu-comps comp-weights assignment]))
+
+(defn mixture-mem
+  "The same model as in mixture, but simplified using mem to reuse random choices."
+  [data]
+  (with-monad probabilistic-sampling-m
+    (if (empty? data)
+      (m-result [{} [] ()])
+      (domonad
+       [comp-weights (mem (dirichlet [10 10 10])) ; mem ensures that the component proportions
+	                                          ; are the same for each data point
+	comp (discrete {:a (nth comp-weights 0), :b (nth comp-weights 1), :c (nth comp-weights 2)})
+	mu-comp (mem (gaussian 0 15) comp) ; similarly the means of the same component are shared
+	obs (cond-data (gaussian mu-comp 1) (first data)) ; condition on the observed data
+	[other-mus cws assignment] (mixture-mem (rest data))]
+       [(assoc other-mus comp mu-comp) comp-weights (cons comp assignment)]))))
+
+(defn test-mixture [model]
+  (let [data-plot (histogram data :title "Dataset" :nbins 50 :density true)
+	samples  (take 200 (drop 8000 (sample-traces (model data)))) ; burn-in of 8000 samples
+	comp-mus (map first samples)
+	comp-weights (map second samples)]
+    (let [xs (range -10 10 0.01)
+	  ;; average over the last samples ... even though this should not be done!!!
+	  weights (map mean (transpose comp-weights))
+	  mus (map mean (transpose (for [mus comp-mus] [(:a mus) (:b mus) (:c mus)])))]
+      (doto data-plot
+	(add-lines xs (map (fn [x] (* (nth weights 0) (pdf-normal x :mean (nth mus 0)))) xs))
+	(add-lines xs (map (fn [x] (* (nth weights 1) (pdf-normal x :mean (nth mus 1)))) xs))
+	(add-lines xs (map (fn [x] (* (nth weights 2) (pdf-normal x :mean (nth mus 2)))) xs))
+	view)
+      [weights mus])))
+
+(comment
+  (test-mixture mixture)
+  (test-mixture mixture-mem))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Finally, extend this to a Dirichlet process such that also the number of
+;;; components is learned
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; first a Beta distributed choice point is needed
+(defn beta [alpha beta]
+  (make-choice-point "BETA" [alpha beta]
+		     (fn [alpha beta] (sample-beta 1 :alpha alpha :beta beta))
+		     (fn [x alpha beta] (Math/log (pdf-beta x :alpha alpha :beta beta)))))
+
+;; this implements the stick breaking construction
+;; note the use of mem to reuse randomness
+(defn pick-a-stick [alpha idx]
+  (domonad probabilistic-sampling-m
+    [stick (mem (beta 1 alpha) idx)
+     stop  (flip stick)
+     num (if stop
+	   (m-result idx)
+	   (pick-a-stick alpha (inc idx)))]
+    num))
+
+(defn dirichlet-process
+  "A Dirichlet process with parameter alpha and base measure base."
+  [alpha base]
+  (domonad probabilistic-sampling-m
+    [idx (pick-a-stick alpha 1)
+     val (mem base idx)]
+    val))
+
+(defn mixture-DP
+  "A Gaussian Dirichlet process mixture model."
+  [alpha data]
+  (with-monad probabilistic-sampling-m
+    (if (empty? data)
+      (m-result [0 {} ()])
+      (domonad
+       [mu-comp (dirichlet-process alpha (gaussian 0 15))
+	obs (cond-data (gaussian mu-comp 1) (first data))
+	[num-comp comp-mus assignment] (mixture-DP alpha (rest data))]
+       (let [comp-idx (if (contains? comp-mus mu-comp)
+			(comp-mus mu-comp)
+			(inc num-comp))
+	     comp-mus (assoc comp-mus mu-comp comp-idx)]
+	 [(count comp-mus) comp-mus (cons comp-idx assignment)])))))
+
+(defn plot-DP-fit [data res]
+  (let [data-plot (histogram data :nbins 50 :density true)
+	[num-comp mus assignment] res
+	weights (density assignment)
+	xs (range -15 15 0.01)]
+    (doseq [[mu idx] (seq mus)]
+      (add-lines data-plot xs (map (fn [x] (* (weights idx) (pdf-normal x :mean mu))) xs)))
+    (add-lines data-plot xs (map (fn [x] (reduce + (for [[mu idx] (seq mus)]
+						     (* (weights idx) (pdf-normal x :mean mu)))))
+				 xs))
+    (view data-plot)))
+
+(defn test-DP [alpha n]
+  (let [res (sample-traces (mixture-DP alpha data))]
+    (loop [x res, i 0]
+      (when (not (> i n))
+	(when (= (mod i 2500) 0)
+	  (plot-DP-fit data (first x)))
+	(recur (rest x) (inc i))))))
+
+(comment
+  (test-DP 1.0 7500))
