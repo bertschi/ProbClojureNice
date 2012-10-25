f88f712ec18bf5f2fe27e9fac2f908ab116959ba
Nils Bertschinger <nils.bertschinger@mis.mpg.de>
2012-10-25 18:30:49 +0200

    Switched to nrepl and trying codox ... (inc version-number)

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject probnice "0.5.0"
-  :description "Probability monad for Metropolis Hastings sampling."
+(defproject probnice "0.7.5"
+  :description "Embedded probabilistic programming in Clojure."
   :url "https://github.com/bertschi/ProbClojureNice"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/algo.monads "0.1.0"]
                  [incanter "1.3.0-SNAPSHOT"]
 		 [org.apache.commons/commons-math "2.2"]]
-  :dev-dependencies [[swank-clojure "1.3.3"]])
+  :plugins [[codox "0.6.1"]
+            [org.clojure/tools.nrepl "0.2.0-beta10"]])
 
